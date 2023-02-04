2b31b66bc641dc46da6596642ace31db5a7b720e
sreeram9061 <sreeramg043@gmail.com>
2023-02-04 10:42:17 -0700

    first

---
+++ index.js
@@ -0,0 +1,7 @@
+let toogu =document.querySelector('.toogle-logo')
+let menue =document.querySelector('.menu')
+
+toogu.addEventListener('click',function () {
+    menue.classList.toggle('is-active')
+    
+})
